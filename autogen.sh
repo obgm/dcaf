3f260b4eadc8a444c9352da22489d64830d050a4
Olaf Bergmann <bergmann@tzi.org>
2016-03-30 15:37:16 +0200

    autogen.sh: fix cn-cbor dependency
    Before invoking autoreconf, this script now checks if the local
    version of cn-cbor is available and calls its build script on success.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,12 @@
 #! /bin/sh -e
 
 srcdir=`dirname "$0"`
+cbordir="$srcdir/ext/cn-cbor"
+
+if test -x "$cbordir/build.sh"; then
+    (cd "$cbordir" && ./build.sh)
+else
+    echo "Error: cannot find cn-cbor" >&2
+fi
 
 autoreconf --force --install --verbose "$srcdir"
