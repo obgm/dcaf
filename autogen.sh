57dd0fcdcbacad61faffb744de99fba3f7f14746
Olaf Bergmann <bergmann@tzi.org>
2017-10-13 15:33:25 +0200

    Makefile.am, configure.ac: fix build dependencies
    This change removes local dependencies in favor of pkg-config so
    that globally installed libcoap and cn-cbor are used.

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,18 @@
 #! /bin/sh -e
 
 srcdir=`dirname "$0"`
-cbordir="$srcdir/ext/cn-cbor"
 
-if test -x "$cbordir/build.sh"; then
-    (cd "$cbordir" && ./build.sh)
-else
-    echo "Error: cannot find cn-cbor" >&2
+GENERATED_FILES="aclocal.m4 ar-lib compile depcomp src/.dirstamp
+               Makefile Makefile.in examples/Makefile examples/Makefile.in
+               config.status configure config.log
+               dcaf_config.h dcaf_config.h.in"
+
+GENERATED_DIRS="autom4te.cache src/.deps"
+
+if test "x$1" = "x--clean"; then
+    rm -f $GENERATED_FILES
+    rm -rf $GENERATED_DIRS
+    exit 0
 fi
 
 autoreconf --force --install --verbose "$srcdir"
