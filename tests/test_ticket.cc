055ef218345bd9b47b8db0a32976593c909221e2
Olaf Bergmann <bergmann@tzi.org>
2018-03-20 12:36:26 +0100

    test_ticket.cc: re-added ticket format tests as index was broken earlier

--- tests/test_ticket.cc
+++ tests/test_ticket.cc
@@ -11,6 +11,7 @@
 #include <memory>
 #include <functional>
 
+#include "dcaf/ace.h"
 #include "dcaf/dcaf.h"
 #include "dcaf/dcaf_int.h"
 #include "dcaf/dcaf_prng.h"
@@ -18,10 +19,28 @@
 #include "test.hh"
 #include "catch.hpp"
 
+/**
+ * Checks if the numeric @p option exists in @p pdu and is equal to
+ * @p value and returns true if this is the case, false otherwise.
+ */
+static bool
+test_option_eq(const coap_pdu_t *pdu, unsigned int option, long value) {
+  coap_opt_iterator_t oi;
+  coap_opt_t *opt = coap_check_option((coap_pdu_t *)pdu, option, &oi);
+  coap_option_t parsed;
+
+  if (opt && (coap_opt_parse(opt, coap_opt_size(opt), &parsed) > 0)) {
+    return value == coap_decode_var_bytes(parsed.value, parsed.length);
+  } else {
+    return false;
+  }
+}
+
 SCENARIO( "DCAF ticket request", "[ticket]" ) {
   static std::unique_ptr<dcaf_authz_t, Deleter> authz;
   static std::unique_ptr<coap_pdu_t, Deleter> coap_pdu;
   static std::unique_ptr<coap_pdu_t, Deleter> coap_response;
+  static std::unique_ptr<cn_cbor, Deleter> claim;
 
   coap_response.reset(coap_pdu_init(0, 0, 0, COAP_DEFAULT_MTU));
 
@@ -70,6 +89,59 @@ SCENARIO( "DCAF ticket request", "[ticket]" ) {
       THEN("a ticket grant can be created") {
         dcaf_set_ticket_grant(&session, authz.get(), coap_response.get());
         REQUIRE(coap_response.get()->code == COAP_RESPONSE_CODE(201));
+
+        /* check Content-Format */
+        REQUIRE(test_option_eq(coap_response.get(),
+                               COAP_OPTION_CONTENT_FORMAT,
+                               authz.get()->mediatype));
+
+        /* check CBOR payload */
+        size_t databuf_len;
+        unsigned char *databuf;
+        cn_cbor *data;
+        REQUIRE(coap_get_data(coap_response.get(), &databuf_len, &databuf));
+
+        data = cn_cbor_decode(databuf, databuf_len, nullptr);
+        REQUIRE(data != nullptr);
+
+        claim.reset(data);
+      }
+    }
+
+    WHEN("A ticket grant was created") {
+      REQUIRE(claim.get() != nullptr);
+
+      THEN("The profile must be coap_dtls") {
+        cn_cbor *profile = cn_cbor_mapget_int(claim.get(), ACE_CLAIM_PROFILE);
+        REQUIRE(profile != nullptr);
+        REQUIRE(profile->type == CN_CBOR_UINT);
+        REQUIRE(profile->v.uint == ACE_PROFILE_DTLS);
+      }
+    }
+
+    WHEN("A ticket grant was created") {
+      REQUIRE(claim.get() != nullptr);
+
+      THEN("The grant must contain a cnf claim with a symmetric key") {
+        cn_cbor *cnf = cn_cbor_mapget_int(claim.get(), ACE_CLAIM_CNF);
+        REQUIRE(cnf != nullptr);
+        REQUIRE(cnf->type == CN_CBOR_MAP);
+
+        cn_cbor *kty = cn_cbor_mapget_int(cnf, COSE_KEY_KTY);
+        REQUIRE(kty != nullptr);
+        REQUIRE(kty->type == CN_CBOR_UINT);
+        REQUIRE(kty->v.uint == COSE_KEY_KTY_SYMMETRIC);
+
+        cn_cbor *k = cn_cbor_mapget_int(cnf, COSE_KEY_K);
+        REQUIRE(k != nullptr);
+        REQUIRE(k->type == CN_CBOR_BYTES);
+
+        /* use the same not-so-random key sequence */
+        uint8_t key[16];
+        dcaf_prng(key, sizeof(key));
+
+        REQUIRE(k->length == sizeof(key));
+        REQUIRE(memcmp(k->v.bytes, key, k->length) == 0);
       }
     }
 
