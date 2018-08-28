f5d343bc7ac3016453f4e26df63e614ad8b4a859
tiff <tiff@athina>
2018-08-28 14:40:49 +0200

    dcaf_crypto_tinydtls.c: added missing stub functions

--- src/dcaf_crypto_tinydtls.c
+++ src/dcaf_crypto_tinydtls.c
@@ -14,10 +14,31 @@
 bool
 dcaf_encrypt(const dcaf_crypto_param_t *params,
              const uint8_t *data, size_t data_len,
+             const uint8_t *aad, size_t aad_len,
              uint8_t *result, size_t *max_result_len) {
   (void)params;
   (void)data;
   (void)data_len;
+  (void)aad;
+  (void)aad_len;
+  (void)result;
+  (void)max_result_len;
+
+  /* FIXME */
+
+  return false;
+}
+
+bool
+dcaf_decrypt(const dcaf_crypto_param_t *params,
+	     const uint8_t *data, size_t data_len,
+	     const uint8_t *aad, size_t add_len,
+	     uint8_t *result, size_t *max_result_len);
+  (void)params;
+  (void)data;
+  (void)data_len;
+  (void)aad;
+  (void)aad_len;
   (void)result;
   (void)max_result_len;
 
