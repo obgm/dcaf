f31ec76c7336dfd445d113f29ac22b29d8edbe0e
Olaf Bergmann <bergmann@tzi.org>
2018-09-20 17:19:33 +0200

    dcaf_coap.[hc]: accessor functions for CoAP method and resource URI

--- src/dcaf_coap.c
+++ src/dcaf_coap.c
@@ -53,12 +53,50 @@ coap_get_content_format(const coap_pdu_t *pdu) {
 
 const uint8_t *
 coap_get_token(const coap_pdu_t *pdu) {
+  assert(pdu);
   return pdu->token;
 }
 
 size_t
 coap_get_token_length(const coap_pdu_t *pdu) {
+  assert(pdu);
   return pdu->token_length;
+}
+
+uint8_t
+coap_get_method(const coap_pdu_t *pdu) {
+  assert(pdu);
+  return pdu->code;
+}
+
+int
+coap_get_resource_uri(const coap_pdu_t *pdu,
+                      uint8_t *buf, size_t *buf_len,
+                      int flags) {
+  coap_string_t *uri;
+  int result = 0;
+  (void)flags;
+
+  assert(pdu);
+  assert(buf);
+  assert(buf_len);
+
+  uri = coap_get_uri_path(pdu);
+  if (!uri) { /* This is an error */
+    *buf_len = 0;
+    return 0;
+  }
+
+  if (uri->length <= *buf_len) {   /* copy entire URI */
+    memcpy(buf, uri->s, uri->length);
+    result = *buf_len = uri->length;
+  } else {                      /* copy only *buf_len bytes */
+    memcpy(buf, uri->s, *buf_len);
+    result = 0;
+  }
+
+  coap_delete_string(uri);
+  return result;
 }
 
 #endif /* RIOT_VERSION */
