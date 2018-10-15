0a37fc8c9f2770265022a5eb3afbd348ce5158cf
Olaf Bergmann <bergmann@tzi.org>
2018-10-15 12:45:23 +0200

    dcaf_key.[hc]: key storage implementation

--- include/dcaf/dcaf_key.h
+++ include/dcaf/dcaf_key.h
@@ -16,6 +16,9 @@
 struct dcaf_key_t;
 typedef struct dcaf_key_t dcaf_key_t;
 
+struct dcaf_keystore_t;
+typedef struct dcaf_keystore_t dcaf_keystore_t;
+
 typedef enum {
   DCAF_NONE = 0,
   DCAF_AES_128 = 1,             /**< AES-128-CCM */
@@ -65,5 +68,49 @@ bool dcaf_key_rnd(dcaf_key_t *key);
  * @return true on sucess, false otherwise.
  */
 bool dcaf_set_key(dcaf_key_t *key, const uint8_t *data, size_t data_len);
+
+struct dcaf_context_t;
+/**
+ * Adds the specified @p key to the key storage of
+ * @p dcaf_context.
+ *
+ * @param dcaf_context The DCAF context to store the
+              *        @p key.
+ * @param peer         The entity with which @p key is
+ *                     associated.
+ * @param peer_length  The actual length of @p peer in
+ *                     bytes.
+ * @param key          The key object to store.
+ */
+void dcaf_add_key(struct dcaf_context_t *dcaf_context,
+                  const uint8_t *peer, size_t peer_len,
+                  dcaf_key_t *key);
+
+/**
+ * Retrieves a key object that has been stored for
+ * the given @p peer in @p dcaf_context. This function
+ * returns the associated dcaf_key_t object if found
+ * or NULL if no key was associated with peer.
+ *
+ * @param dcaf_context   The DCAF context where to search
+ *                       for the key.
+ * @param peer           The identifier of the peer for
+ *                       which to lookup the key.
+ * @param peer_length    The actual length of @p peer
+ *                       in bytes.
+ * @param kid            The key identifier as defined
+ *                       for this key by @p peer. If
+ *                       set to NULL, any key identifier
+ *                       will match during search.
+ * @param kid_length     The actual length of @p kid.
+ *                       Must be set to 0 if kid is NULL.
+ * @return The key stored for @p peer or NULL if no key
+ *         was found.
+ */
+dcaf_key_t *dcaf_find_key(struct dcaf_context_t *dcaf_context,
+                          const uint8_t *peer,
+                          size_t peer_length,
+                          const uint8_t *kid,
+                          size_t kid_length);
 
 #endif /* _DCAF_KEY_H_ */
