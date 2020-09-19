7dfe3fd33dc41373ad484e70984d65bf6b789bab
Olaf Bergmann <bergmann@tzi.org>
2020-09-19 21:42:32 +0200

    dcaf_prng.c: set default PRNG function for ESP32
    When the pre-processor symbol ESP_PLATFORM is defined, the default
    pseudo random number generation function is set to esp_fill_random().

--- src/dcaf_prng.c
+++ src/dcaf_prng.c
@@ -1,8 +1,8 @@
 /*
  * dcaf_prng.c -- random number generation
  *
- * Copyright (C) 2015-2017 Olaf Bergmann <bergmann@tzi.org>
- *               2015-2017 Stefanie Gerdes <gerdes@tzi.org>
+ * Copyright (C) 2015-2020 Olaf Bergmann <bergmann@tzi.org>
+ *               2015-2020 Stefanie Gerdes <gerdes@tzi.org>
  *
  * This file is part of the DCAF library libdcaf. Please see README
  * for terms of use.
@@ -10,7 +10,14 @@
 
 #include "dcaf/dcaf_prng.h"
 
-static dcaf_rand_func_t rand_func = NULL;
+#ifdef ESP_PLATFORM
+#include <esp_system.h>
+#define PRNG_FUNC ((dcaf_rand_func_t)esp_fill_random)
+#else
+#define PRNG_FUNC (NULL)
+#endif /* ESP_PLATFORM */
+
+static dcaf_rand_func_t rand_func = PRNG_FUNC;
 
 void
 dcaf_set_prng(dcaf_rand_func_t rng) {
