9e3fb9b1d7642f119600713ead326e735bf3888b
Olaf Bergmann <bergmann@tzi.org>
2020-09-29 21:29:56 +0200

    ace.h: update CBOR numeric option numbers
    * CBOR option numbers for ACE fields and error codes have been updated
      to draft-ietf-ace-oauth-authz-33 and draft-ietf-ace-oauth-params-13.
    * Cleanup of unused definitions.
    * New configuration parameter CONFIG_ACE_REQUEST_PROFILE. If set to
      a value != 0, the client will include an empty ace_profile parameter
      in the token request.

--- include/dcaf/ace.h
+++ include/dcaf/ace.h
@@ -1,7 +1,7 @@
 /*
  * ace.h -- specific definitions for ACE coap_dtls profile
  *
- * Copyright (C) 2017 Olaf Bergmann <bergmann@tzi.org>
+ * Copyright (C) 2017-2020 Olaf Bergmann <bergmann@tzi.org>
  *
  * This file is part of the DCAF library libdcaf. Please see README
  * for terms of use.
@@ -9,11 +9,6 @@
 
 #ifndef _ACE_H_
 #define _ACE_H_ 1
-
-#define ACE_TOKEN_POP  2
-
-#define ACE_ASINFO_AS            0
-#define ACE_ASINFO_NONCE         5
 
 /** OAuth grant types as defined in draft-ietf-ace-oauth-authz */
 enum ace_grant_type {
@@ -29,14 +24,21 @@ enum ace_profile {
   ACE_PROFILE_OSCORE = 2        /* draft-ietf-ace-oscore-profile */
 };
 
+#ifdef CONFIG_ACE_REQUEST_PROFILE
+#define ACE_REQUEST_PROFILE CONFIG_ACE_REQUEST_PROFILE
+#else  /* !CONFIG_ACE_REQUEST_PROFILE */
+#define ACE_REQUEST_PROFILE 0
+#endif /* !CONFIG_ACE_REQUEST_PROFILE */
+
 /* CBOR mappings for token request and response fields according to
- * draft-ietf-ace-oauth-authz-33
+ * draft-ietf-ace-oauth-authz-33, draft-ietf-ace-oauth-params-13
  */
 
 enum ace_msg {
   ACE_MSG_ACCESS_TOKEN      = 1,
   ACE_MSG_EXPIRES_IN        = 2,
   ACE_MSG_AUDIENCE          = 3,
+  ACE_MSG_CNF               = 8,
   ACE_MSG_SCOPE             = 9,
   ACE_MSG_CLIENT_ID         = 24,
   ACE_MSG_CLIENT_SECRET     = 25,
@@ -53,7 +55,8 @@ enum ace_msg {
   ACE_MSG_PASSWORD          = 36,
   ACE_MSG_REFRESH_TOKEN     = 37,
   ACE_MSG_PROFILE           = 38,
-  ACE_MSG_CNONCE            = 39
+  ACE_MSG_CNONCE            = 39,
+  ACE_MSG_RS_CNF            = 41,
 };
 
 /* CBOR values for ACE AS Request Creation Hints according to
@@ -69,13 +72,14 @@ enum ace_req_creation_hints {
 
 /** Error codes from ACE framework */
 enum ace_error_code {
-  ACE_ERROR_INVALID_REQUEST        = 0,
-  ACE_ERROR_INVALID_CLIENT         = 1,
-  ACE_ERROR_INVALID_GRANT          = 2,
-  ACE_ERROR_UNAUTHORIZED_CLIENT    = 3,
-  ACE_ERROR_UNSUPPORTED_GRANT_TYPE = 4,
-  ACE_ERROR_INVALID_SCOPE          = 5,
-  ACE_ERROR_UNSUPPORTED_POP_KEY    = 6
+  ACE_ERROR_INVALID_REQUEST        = 1,
+  ACE_ERROR_INVALID_CLIENT         = 2,
+  ACE_ERROR_INVALID_GRANT          = 3,
+  ACE_ERROR_UNAUTHORIZED_CLIENT    = 4,
+  ACE_ERROR_UNSUPPORTED_GRANT_TYPE = 5,
+  ACE_ERROR_INVALID_SCOPE          = 6,
+  ACE_ERROR_UNSUPPORTED_POP_KEY    = 7,
+  ACE_ERROR_INCOMPATIBLE_PROFILES  = 8,
 };
 
 #endif /* _ACE_H_ */
